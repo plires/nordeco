02c2ea7300c12b9e8c6ddacbf6f0a6e5eb5d93ca
Pablo Lires <pablolires@gmail.com>
2023-02-13 12:11:22 -0300

    Agregado de seccion Tetovinilico al sitio

---
+++ cielorrasos-premium-tetovinilico-argentina.php
@@ -0,0 +1,435 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang="es" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="es">
+<!--<![endif]-->
+
+<head>
+  <!-- Tag Manager Head-->
+  <?php include_once("includes/tag_manager_head.php") ?>
+  <meta charset="utf-8">
+  <title>Nordeco, Cielorrasos Premium - Tetovinilico Argentina</title>
+  <meta name="description"
+    content="Combinando el concepto de Diseño con la calidad y funcionalidad, presentamos las nuevas líneas de Revestimientos Vinílicos para Techo.">
+  <meta name="author" content="Libre">
+
+  <!-- Mobile Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="images/favicon.ico">
+
+  <!-- Equipo -->
+  <link type="text/plain" rel="author" href="humans.txt" />
+
+  <!-- Web Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,500,500italic,700,700italic'
+    rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
+
+  <!-- Bootstrap core CSS -->
+  <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+  <!-- Font Awesome CSS -->
+  <link href="fonts/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+  <!-- Fontello CSS -->
+  <link href="fonts/fontello/css/fontello.css" rel="stylesheet">
+
+  <!-- Plugins -->
+  <link href="plugins/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="plugins/rs-plugin/css/settings.css" rel="stylesheet">
+  <link href="css/animations.css" rel="stylesheet">
+  <link href="plugins/owl-carousel/owl.carousel.css" rel="stylesheet">
+  <link href="plugins/owl-carousel/owl.transitions.css" rel="stylesheet">
+  <link href="plugins/hover/hover-min.css" rel="stylesheet">
+  <link href="plugins/morphext/morphext.css" rel="stylesheet">
+  <!-- Plugin Slick (Carrousel logos) CSS -->
+  <link rel="stylesheet" type="text/css" href="plugins/slick.css">
+  <link rel="stylesheet" type="text/css" href="plugins/slick-theme.css">
+
+  <!-- The Project core CSS file -->
+  <link href="css/style.css" rel="stylesheet">
+  <!-- Color Scheme (In order to change the color scheme, replace the blue.css with the color scheme that you prefer)-->
+  <link href="css/skins/light_blue.css" rel="stylesheet">
+
+  <!-- Custom css -->
+  <link href="css/custom.css" rel="stylesheet">
+
+  <!-- Chatra {literal} -->
+  <script>
+  (function(d, w, c) {
+    w.ChatraID = 'pk5jBQAMGW9CM47ki';
+    var s = d.createElement('script');
+    w[c] = w[c] || function() {
+      (w[c].q = w[c].q || []).push(arguments);
+    };
+    s.async = true;
+    s.src = 'https://call.chatra.io/chatra.js';
+    if (d.head) d.head.appendChild(s);
+  })(document, window, 'Chatra');
+  </script>
+  <!-- /Chatra {/literal} -->
+</head>
+
+<body class="no-trans front-page   ">
+  <!-- Tag Manager -->
+  <?php include_once("includes/tag_manager_body.php") ?>
+
+  <?php $product="cielorrasos" ?>
+  <?php $current="teto" ?>
+
+  <!-- scrollToTop -->
+  <!-- ================ -->
+  <div class="scrollToTop circle"><i class="icon-up-open-big"></i></div>
+
+  <!-- page wrapper start -->
+  <!-- ================ -->
+  <?php include ("includes/header.inc"); ?>
+
+  <section class="revestimientos tetovinilico">
+
+    <div id="page-start"></div>
+
+    <!-- Header Img -->
+    <section class="header container-fluid">
+      <div class="row">
+        <div class="col-sm-12">
+          <img src="images/tetovinilico/header-tetovinilico.jpg" alt="cielorrasos premium tetovinilico argentina">
+          <div class="contenido_header">
+            <div class="row">
+              <div class="container">
+                <div class="col-md-12">
+                  <h1>TETO Vinílico Argentina</h1>
+                  <div class="separator"></div>
+                  <p>
+                    Un nuevo concepto en revestimientos para techos llegó a Argentina se trata de TETO Vinílico, un
+                    revestimiento de calidad PREMIUM con materiales reciclados y un acabado sorprendente.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Header Img end -->
+
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>TETO VINÍLICO ARGENTINA</h1>
+        </div>
+      </div>
+    </div>
+
+    <!-- Faja Verde -->
+    <section class="verde-bg texto_blanco pv-30 clearfix">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="text-center">UN <strong>NUEVO CONCEPTO</strong></h2>
+            <div class="separator"></div>
+            <p class="large text-center">
+              Combinando el concepto de Diseño con la calidad y funcionalidad, presentamos las nuevas líneas de
+              Revestimientos Vinílicos para Techo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Faja Verde end -->
+
+    <!-- Seccion Cielorrasos -->
+    <section class="siding perfiles cielorrasos container">
+
+      <div class="row">
+
+        <div class="col-sm-12 text-center">
+
+          <p class="descripcion_wall_panel desc_parrafo_teto">
+            Apostando por la innovación, la tecnología y la sostenibilidad.
+          </p>
+
+          <p class="descripcion_wall_panel desc_parrafo_teto">
+            Con un producto 100% reciclable, cero costo de mantenimiento y posibilitando un trabajo limpio.
+            Nuestros productos son fabricados teniendo profundo respeto por el medio ambiente tanto en los procesos como
+            en la elección de los materiales.
+          </p>
+
+          <p class="descripcion_wall_panel desc_parrafo_teto">
+            Disponibles en 3 líneas: Origens, Woods y Plus.
+          </p>
+
+          <p class="descripcion_wall_panel desc_parrafo_teto">
+            Combinando gráficos llamativos y coloridos, con la textura natural de la madera y la rusticidad del cemento
+            existe un modelo para cada estilo arquitectónico, cada línea posee varios tonos y texturas.
+          </p>
+
+        </div>
+
+        <div class="col-sm-12 text-center">
+          <img class="tablas" src="images/tetovinilico/teto-tablas.jpg" alt="tetovinilicos tablas">
+          <p class="descripcion_wall_panel desc_parrafo_teto">
+            <strong>
+              Es un producto PREMIUM, con un cuidado estético impecable, fácil de instalar, formado por 4 capas que
+              forman
+              una placa compacta y altamente resistente.
+            </strong>
+          </p>
+        </div>
+
+      </div>
+
+      <div class="row mb-10 text-center">
+
+        <div class="card col-sm-6 text-center mb-20">
+          <img src="images/tetovinilico/teto-a.jpg" alt="cielorraso premium tetovinilico -a">
+        </div>
+
+        <div class="card col-sm-6 text-center mb-20">
+          <img src="images/tetovinilico/teto-b.jpg" alt="cielorraso premium tetovinilico -b">
+        </div>
+
+        <div class="card col-sm-6 text-center mb-20">
+          <img src="images/tetovinilico/teto-c.jpg" alt="cielorraso premium tetovinilico -c">
+        </div>
+
+        <div class="card col-sm-6 text-center mb-20">
+          <img src="images/tetovinilico/teto-d.jpg" alt="cielorraso premium tetovinilico -d">
+        </div>
+
+        <div class="card col-sm-6 text-center mb-20">
+          <img src="images/tetovinilico/teto-e.jpg" alt="cielorraso premium tetovinilico -e">
+        </div>
+
+        <div class="card col-sm-6 text-center mb-20">
+          <img src="images/tetovinilico/teto-f.jpg" alt="cielorraso premium tetovinilico -f">
+        </div>
+
+      </div>
+
+    </section>
+    <!-- Seccion Cielorrasos end -->
+
+    <!-- Medidas -->
+    <section class="container medidas">
+      <div class="row mb-20">
+        <div class="col-md-12">
+          <h2 class="large text-center">ESPECIFICACIONES TÉCNICAS</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 detalle">
+          <p>MEDIDA: 200mm x 5800mm x 8mm</p>
+          <p>PAQUETE: 10 placas (11,6 m2)</p>
+          <p>MONTAJE: Macho/hembra</p>
+          <p>ACABADO: Barniz mate</p>
+          <p>ACCESORIOS: Remates</p>
+          <p>LIMPIEZA: Paño húmedo con</p>
+          <p>solución de 50% agua y 50% alcohol</p>
+          <p>EXPOSICIÓN SOLAR: No indicado</p>
+        </div>
+        <div class="col-sm-6">
+          <img src="./images/tetovinilico/medidas.gif" alt="medidas tabla tetovinilico argentina">
+        </div>
+      </div>
+    </section>
+    <!-- Medidas end -->
+
+    <!-- Faja Verde -->
+    <section class="verde-bg texto_blanco pv-30 clearfix">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="text-center">Instalá <strong>EL TECHO DEL FUTURO.</strong></h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Faja Verde end -->
+
+    <!-- Seccion Caracteristicas -->
+    <section class="caracteristicas container text-center">
+      <div class="row">
+        <div class="col-xs-6 col-sm-3 descripcion">
+          <img src="images/revestimientos/resistentes-agua.gif" alt="revestimientos de pared resistentes al agua">
+          <h4>Resistentes<br>al agua</h4>
+        </div>
+        <div class="col-xs-6 col-sm-3 descripcion">
+          <img src="images/revestimientos/resistente-termitas.gif"
+            alt="revestimientos de pared resistentes a las termitas">
+          <h4>Resistentes<br>a termitas</h4>
+        </div>
+        <div class="col-xs-6 col-sm-3 descripcion">
+          <img src="images/revestimientos/sin-mantenimiento.gif" alt="revestimientos de pared libre de mantenimiento">
+          <h4>No necesitan pintura<br>ni mantenimiento </h4>
+        </div>
+        <div class="col-xs-6 col-sm-3 descripcion">
+          <img src="images/revestimientos/facil-instalacion.gif" alt="revestimientos de pared facil instalacion">
+          <h4>Fácil<br>Instalación</h4>
+        </div>
+      </div>
+    </section>
+    <!-- Seccion Caracteristicas end -->
+
+    <!-- Galeria imagenes revestimientos -->
+    <?php include ("includes/galeria-imagenes-teto.inc"); ?>
+
+    <!-- section verde start -->
+    <!-- ================ -->
+    <section class="section fondo_verde clearfix">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-8">
+            <h2 class="color_blanco">¿NECESITÁS MÁS INFORMACIÓN?</h2>
+          </div>
+          <div class="col-md-4">
+            <br>
+            <p><a href="http://nordeco.com.ar/contacto.php" class="btn btn-verde btn-animated">CONSULTAR<i
+                  class="pl-10 fa fa-arrow-right"></i></a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- section verde end -->
+
+  </section>
+
+  <br>
+
+  <!-- Footer -->
+  <?php include ("includes/footer.inc"); ?>
+
+  </div>
+  <!-- page-wrapper end -->
+
+  <!-- JavaScript files placed at the end of the document so the pages load faster -->
+  <!-- ================================================== -->
+  <!-- Jquery and Bootstap core js files -->
+  <script type="text/javascript" src="plugins/jquery.min.js"></script>
+  <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+  <!-- Modernizr javascript -->
+  <script type="text/javascript" src="plugins/modernizr.js"></script>
+  <!-- jQuery Revolution Slider  -->
+  <script type="text/javascript" src="plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+  <script type="text/javascript" src="plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+  <!-- Isotope javascript -->
+  <script type="text/javascript" src="plugins/isotope/isotope.pkgd.min.js"></script>
+  <!-- Magnific Popup javascript -->
+  <script type="text/javascript" src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+  <!-- Appear javascript -->
+  <script type="text/javascript" src="plugins/waypoints/jquery.waypoints.min.js"></script>
+  <!-- Count To javascript -->
+  <script type="text/javascript" src="plugins/jquery.countTo.js"></script>
+  <!-- Parallax javascript -->
+  <script src="plugins/jquery.parallax-1.1.3.js"></script>
+  <!-- Contact form -->
+  <script src="plugins/jquery.validate.js"></script>
+  <!-- Morphext -->
+  <script type="text/javascript" src="plugins/morphext/morphext.min.js"></script>
+  <!-- Background Video -->
+  <script src="plugins/vide/jquery.vide.js"></script>
+  <!-- Owl carousel javascript -->
+  <script type="text/javascript" src="plugins/owl-carousel/owl.carousel.js"></script>
+  <!-- SmoothScroll javascript -->
+  <script type="text/javascript" src="plugins/jquery.browser.js"></script>
+  <script type="text/javascript" src="plugins/SmoothScroll.js"></script>
+  <!-- Initialization of Plugins -->
+  <script type="text/javascript" src="js/template.js"></script>
+  <!-- Custom Scripts -->
+  <script type="text/javascript" src="js/custom.js"></script>
+
+  <!-- Plugin Slick (Carrousel) JS -->
+  <script src="plugins/slick.min.js" type="text/javascript" charset="utf-8"></script>
+
+  <!-- Mailchimp -->
+  <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js"
+    data-dojo-config="usePlainJson: true, isDebug: false"></script>
+  <script type="text/javascript">
+  require(["mojo/signup-forms/Loader"], function(L) {
+    L.start({
+      "baseUrl": "mc.us17.list-manage.com",
+      "uuid": "406cbe29a3e2f9eb2369a881b",
+      "lid": "3c63e69616"
+    })
+  })
+  </script>
+
+  <script type="text/javascript">
+  $('#gallery1').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+  $('#gallery2').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+  $('#gallery3').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+  $('#gallery4').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+  $('#gallery5').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+  $('#gallery6').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+  $('#gallery7').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+  </script>
+
+</body>
+
+</html>
