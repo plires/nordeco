43c2bd803956cb3040d7a5d4df69b1da0e1c9c77
Pablo Lires <pablolires@gmail.com>
2023-09-29 20:06:09 -0300

    Cambios de configuracion para landing pisos vinilicos de ofertas

--- landing-2018/pisos-vinilicos-ofertas/gracias.php
+++ landing-2018/pisos-vinilicos-ofertas/gracias.php
@@ -1,159 +1,161 @@
 <!doctype html>
 <html lang="es">
-	<head>
-		<!-- Tag Manager Head -->
-    <?php include_once("../includes/tag_manager_head.inc") ?>
-    
-	  <!-- Required meta tags -->
-	  <meta charset="utf-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	  <meta name="description" content="Pisos vinilicos resistentes al agua para entornos domesticos y comerciales.">
 
-	  <!-- Favicons -->
-    <link rel="apple-touch-icon" href="img/apple-icon.png">
-    <link rel="icon" href="img/favicon.png">
+<head>
+  <!-- Tag Manager Head -->
+  <?php include_once("../includes/tag_manager_head.inc") ?>
 
-    <!-- Normalize CSS -->
-	  <link rel="stylesheet" type="text/css" href="css/normalize.min.css">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Pisos vinilicos resistentes al agua para entornos domesticos y comerciales.">
 
-	  <!-- Bootstrap Grid CSS -->
-	  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" href="img/apple-icon.png">
+  <link rel="icon" href="img/favicon.png">
 
-	  <!-- Animated CSS -->
-	  <link rel="stylesheet" type="text/css" href="css/animate.css">
+  <!-- Normalize CSS -->
+  <link rel="stylesheet" type="text/css" href="./../css/normalize.min.css">
 
-	  <!-- Google Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+  <!-- Bootstrap Grid CSS -->
+  <link rel="stylesheet" type="text/css" href="./../css/bootstrap.min.css">
 
-	  <!-- Custom CSS -->
-	  <link rel="stylesheet" type="text/css" href="css/app.css">
+  <!-- Animated CSS -->
+  <link rel="stylesheet" type="text/css" href="./../css/animate.css">
 
-	  <title>Pisos Vinilicos</title>
-		
-	</head>
-	
-	<body>
-		<!-- Tag Manager Body -->
-		<?php include_once("../includes/tag_manager_body.inc") ?>
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
-		<!-- HEADER -->
-		<header class="container">
-			<div class="row">
-				<div class="col-md-6 text-center wow fadeIn">
-		      <img src="img/logo-nordeco.png" alt="logo nordeco">
-				</div>
-				<div class="col-md-6 text-right wow fadeIn header_datos hidden_phone">
-		      <a class="transition" href="mailto:info@nordeco.com.ar">info@nordeco.com.ar</a>
-				</div>
-			</div>
-		</header>
-		<!-- HEADER end -->
+  <!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css" href="css/app.css">
 
-		<!-- FORMULARIO MOBILE -->
-		<section class="container mb_30 formulario_mobile">
+  <title>Pisos Vinilicos</title>
 
-			<div class="row">
-				<div class="col-md-12 text-center">
-					<img class="w-100" src="img/pisos-header-mobile.jpg">
-					<h1>ENVIO EXITOSO!</strong></h1>
-					<p class="gracias_parrafo">Gracias por tu envio.</p>
-        	<p class="gracias_parrafo">En breve nos estaremos contactando para contestar todas tus consultas.</p>
-				</div>
-			</div>
+</head>
 
-		</section>
-		<!-- FORMULARIO MOBILE end-->
+<body>
+  <!-- Tag Manager Body -->
+  <?php include_once("../includes/tag_manager_body.inc") ?>
 
-		<!-- FORMULARIO ESCRITORIO -->
-		<section class="container-fluid header_img mb_60 formulario_escritorio">
-			<div class="container">
+  <!-- HEADER -->
+  <header class="container">
+    <div class="row">
+      <div class="col-md-6 text-center wow fadeIn">
+        <img src="img/logo-nordeco.png" alt="logo nordeco">
+      </div>
+      <div class="col-md-6 text-right wow fadeIn header_datos hidden_phone">
+        <a class="transition" href="mailto:info@nordeco.com.ar">info@nordeco.com.ar</a>
+      </div>
+    </div>
+  </header>
+  <!-- HEADER end -->
 
-				<div class="row">
-					<div class="col-md-12">
-						<img class="mb_15 header_garantia" src="img/sistema-click.png" alt="sistema click">
-					</div>
-				</div>
+  <!-- FORMULARIO MOBILE -->
+  <section class="container mb_30 formulario_mobile">
 
-				<div class="row">
-					<div class="col-md-8 offset-md-2 text-center datos_linea">
-						<h1>ENVIO EXITOSO!</strong></h1>
-						<p class="gracias_parrafo">Gracias por tu envio.</p>
-          	<p class="gracias_parrafo">En breve nos estaremos contactando para contestar todas tus consultas.</p>
-					</div>
-				</div>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <img class="w-100" src="img/pisos-header-mobile.jpg">
+        <h1>ENVIO EXITOSO!</strong></h1>
+        <p class="gracias_parrafo">Gracias por tu envio.</p>
+        <p class="gracias_parrafo">En breve nos estaremos contactando para contestar todas tus consultas.</p>
+      </div>
+    </div>
 
-			</div>
-		</section>
-		<!-- FORMULARIO ESCRITORIO end-->
+  </section>
+  <!-- FORMULARIO MOBILE end-->
 
-	
-		<!-- OTRAS LANDING -->
-		<section class="container-fluid mb_60">
+  <!-- FORMULARIO ESCRITORIO -->
+  <section class="container-fluid header_img mb_60 formulario_escritorio">
+    <div class="container">
 
-			<div class="row mb_30">
+      <div class="row">
+        <div class="col-md-12">
+          <img class="mb_15 header_garantia" src="img/sistema-click.png" alt="sistema click">
+        </div>
+      </div>
 
-				<div class="col-md-12 text-center wow fadeInLeft">
-					<h2 class="bold">CONOCÉ OTROS PRODUCTOS</h2>
-				</div>
+      <div class="row">
+        <div class="col-md-8 offset-md-2 text-center datos_linea">
+          <h1>ENVIO EXITOSO!</strong></h1>
+          <p class="gracias_parrafo">Gracias por tu envio.</p>
+          <p class="gracias_parrafo">En breve nos estaremos contactando para contestar todas tus consultas.</p>
+        </div>
+      </div>
 
-			</div>
+    </div>
+  </section>
+  <!-- FORMULARIO ESCRITORIO end-->
 
-			<div class="row">
 
-				<div class="col-md-3 text-center landing wow fadeInUp">
-					<a class="transition" href="../../">
-						<img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-1.jpg" alt="deck ecologico 1">
-					</a>
-				</div>
-				<div class="col-md-3 text-center landing wow fadeInUp">
-					<a class="transition" href="../../">
-						<img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-2.jpg" alt="deck ecologico 2">
-					</a>
-				</div>
-				<div class="col-md-3 text-center landing wow fadeInUp">
-					<a class="transition" href="../../">
-						<img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-3.jpg" alt="deck ecologico 3">
-					</a>
-				</div>
-				<div class="col-md-3 text-center landing wow fadeInUp">
-					<a class="transition" href="../../">
-						<img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-4.jpg" alt="deck ecologico 4">
-					</a>
-				</div>
+  <!-- OTRAS LANDING -->
+  <section class="container-fluid mb_60">
 
-			</div>
-				
+    <div class="row mb_30">
 
-		</section>
-		<!-- OTRAS LANDING end -->
+      <div class="col-md-12 text-center wow fadeInLeft">
+        <h2 class="bold">CONOCÉ OTROS PRODUCTOS</h2>
+      </div>
 
-		<footer class="container-fluid">
-			<div class="container text-center">
-				<div class="row mb_15">
-					<div class="col-md-8 offset-md-2 align_vertical_horizontal">
-						<div class="transition mail">
-							
-							<a href="mailto:info@nordeco.com.ar"><img src="img/mail.png" alt="mail">info@nordeco.com.ar</a>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-8 offset-md-2">
-						<p>Copyright &copy; <?= date('Y'); ?>, All Rights Reserved.</p>
-					</div>
-				</div>
-			</div>
-		</footer>
+    </div>
 
-	  <!-- Optional JavaScript -->
-	  <!-- jQuery primero, luego Popper.js, luego Bootstrap JS -->
-	  <script type="text/javascript" src="js/jquery.min.js"></script>
-	  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <div class="row">
 
-	  <!-- Wow efecto para que aparezcan los objetos cuando se va scroleando -->
-		<script type="text/javascript" src="js/wow.min.js"></script>
+      <div class="col-md-3 text-center landing wow fadeInUp">
+        <a class="transition" href="../../">
+          <img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-1.jpg" alt="deck ecologico 1">
+        </a>
+      </div>
+      <div class="col-md-3 text-center landing wow fadeInUp">
+        <a class="transition" href="../../">
+          <img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-2.jpg" alt="deck ecologico 2">
+        </a>
+      </div>
+      <div class="col-md-3 text-center landing wow fadeInUp">
+        <a class="transition" href="../../">
+          <img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-3.jpg" alt="deck ecologico 3">
+        </a>
+      </div>
+      <div class="col-md-3 text-center landing wow fadeInUp">
+        <a class="transition" href="../../">
+          <img class="img-fluid mb_15" src="img/inspiracion-deck-sintetico-4.jpg" alt="deck ecologico 4">
+        </a>
+      </div>
 
-		<!-- Comunes a todos las paginas -->
-		<script type="text/javascript" src="js/app.js"></script>
-	</body>
+    </div>
+
+
+  </section>
+  <!-- OTRAS LANDING end -->
+
+  <footer class="container-fluid">
+    <div class="container text-center">
+      <div class="row mb_15">
+        <div class="col-md-8 offset-md-2 align_vertical_horizontal">
+          <div class="transition mail">
+
+            <a href="mailto:info@nordeco.com.ar"><img src="img/mail.png" alt="mail">info@nordeco.com.ar</a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <p>Copyright &copy; <?= date('Y'); ?>, All Rights Reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery primero, luego Popper.js, luego Bootstrap JS -->
+  <script type="text/javascript" src="./../js/jquery.min.js"></script>
+  <script type="text/javascript" src="./../js/bootstrap.min.js"></script>
+
+  <!-- Wow efecto para que aparezcan los objetos cuando se va scroleando -->
+  <script type="text/javascript" src="./../js/wow.min.js"></script>
+
+  <!-- Comunes a todos las paginas -->
+  <script type="text/javascript" src="js/app.js"></script>
+</body>
+
 </html>
